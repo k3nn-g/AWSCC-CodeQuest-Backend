9ef98b15fb82a97ca51855dfa2c21760eb37fcf9
Kenneth Guioguio <107411633+k3nn-g@users.noreply.github.com>
2023-11-20 20:56:17 +0800

    Challenge Day 1

---
+++ backend/day-01/ChallengeDay1.py
@@ -0,0 +1,5 @@
+name = "Kenneth"
+department = "Software and Web Development"
+
+print(f"Hello, I am {name}. I love {department} and I love being in the backend even more!")
+print(f"One thing about me is that I am a dedicated student who is will to learn more!")
--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Kenneth"
+age = "19"
+isMale = "true"
+department = "Department of Software and Web Development"
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
