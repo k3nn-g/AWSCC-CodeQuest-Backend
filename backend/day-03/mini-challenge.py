2d42ebe2a28147fd3628130ac9351db25b4fc53b
Kenneth Guioguio <107411633+k3nn-g@users.noreply.github.com>
2023-11-22 11:36:09 +0800

    Day 3 (Mini-Challenge)

---
+++ backend/day-03/mini-challenge.py
@@ -0,0 +1,8 @@
+x = int(input("Give a number: "))
+
+if x < 0:
+    print(f"{x} is a negative number!")
+elif x >= 1:
+    print(f"{x} is a positive number!")
+else:
+    print(f"The number is zero!")
