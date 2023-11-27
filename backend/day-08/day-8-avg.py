e5655dff02a02130880f926828965e61c75df556
Kenneth Guioguio <107411633+k3nn-g@users.noreply.github.com>
2023-11-27 18:07:27 +0800

    Update day-8-avg.py

--- backend/day-08/day-8-avg.py
+++ backend/day-08/day-8-avg.py
@@ -1,10 +1,15 @@
 my_list = [5, 9, 2, 3, 1]
+my_avg = avg(my_list)
+print(my_avg)
 
-sum = 0
-avg = 0
+def avg(numlist):
+    sum = 0
+    avg = 0
 
-for num in my_list:
-    sum += num
+    for num in numlist:
+        sum += num
 
-avg = sum / len(my_list)
-print(avg)
+    avg = sum / len(numlist)
+    print(avg)
+    
+    return avg
